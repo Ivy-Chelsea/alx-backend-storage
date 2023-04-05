8c4c3eb886e4194cd3ed140f06876ef2cb368e34
Ivy-Chelsea <ivychelsea20@gmail.com>
2023-04-05 23:04:30 +0300

    add attribute to users

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,9 @@
+-- Script that creates table users
+-- with attributes id, email, name, country
+CREATE TABLE IF NOT EXISTS users
+(
+	id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	email varchar(255) NOT NULL UNIQUE,
+	name varchar(255),
+	country ENUM('US', 'CO', 'IN') DEFAULT 'US' NOT NULL
+);
