5e80e3812e2decb3070407771a711475ce99fe0a
Ivy-Chelsea <105067998+Ivy-Chelsea@users.noreply.github.com>
2023-04-06 21:52:33 +0300

    users table

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,6 +1,7 @@
 -- Script that creates table users
 -- with attributes id, email, name, country
-CREATE TABLE IF NOT EXISTS users(
+CREATE TABLE IF NOT EXISTS users
+(
 	id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	email varchar(255) NOT NULL UNIQUE,
 	name varchar(255),
