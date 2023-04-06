566cea6d17b478c5bd27e7010c7d6dad0747b1e2
Ivy-Chelsea <ivychelsea20@gmail.com>
2023-04-06 17:16:13 +0300

    validation trigger

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,11 @@
+-- Script that creates trigger that sets attribute valid_email
+-- only when email has been changed
+DELIMITER //;
+CREATE TRIGGER validate BEFORE UPDATE
+ON users
+FOR EACH ROW
+BEGIN
+	IF NEW.email != OLD.email THEN
+		SET NEW.valid_email = 0;
+	END IF; //
+DELIMITER
