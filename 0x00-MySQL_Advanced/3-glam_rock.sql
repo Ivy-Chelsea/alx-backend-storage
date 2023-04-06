39e59aef1b9f478e7bd2cf8440b3f67a4057afb8
Ivy-Chelsea <105067998+Ivy-Chelsea@users.noreply.github.com>
2023-04-06 18:33:37 +0300

    reduce length

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,4 @@
 -- Script that lists all bands with Glam rock as their main style
 -- Ranked by their longevity
-SELECT band_name, COALESCE(split, 2020) - formed as lifespan
-FROM metal_bands
-WHERE style LIKE '%Glam rock%'
-ORDER BY lifespan DESC;
+SELECT band_name, COALESCE(split, 2020) - formed as lifespan FROM metal_bands
+WHERE style LIKE '%Glam rock%' ORDER BY lifespan DESC;
