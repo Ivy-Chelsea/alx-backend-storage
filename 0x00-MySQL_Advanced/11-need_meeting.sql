200e8d29a92a38e8f07b34f6e5385d0265eafff0
Ivy-Chelsea <ivychelsea20@gmail.com>
2023-04-06 17:30:48 +0300

    need meeting view

---
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,7 @@
+/*
+Script that creates view need_meeting
+That lists students that have scored under 80
+And no last_meeting or more than 1 month
+*/
+CREATE VIEW need_meeting AS SELECT name from students WHERE score < 80
+AND (last_meeting IS NULL OR last_meeting < DATE(CURDATE() - INTERVAL 1 MONTH));
