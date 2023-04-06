98f7ebac246bd3a7bb956e8525b6c70f3024a568
Ivy-Chelsea <ivychelsea20@gmail.com>
2023-04-06 16:16:17 +0300

    glam rock content

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,6 @@
+-- Script that lists all bands with Glam rock as their main style
+-- Ranked by their longevity
+SELECT band_name, COALESCE(split, 2020) - formed as lifespan
+FROMM metal_bands
+WHERE style LIKE "Glam rock"
+ORDER BY lifespan DESC;
