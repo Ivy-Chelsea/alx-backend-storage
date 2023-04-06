d32a11360560a47abfcecc258be75425fdc944c2
Ivy-Chelsea <ivychelsea20@gmail.com>
2023-04-06 18:23:17 +0300

    change documentation

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -1,5 +1,3 @@
-/*
-Script that creates index idx_name_first_score
-On table names and first letter of name and the score
-*/
+-- Script that creates index idx_name_first_score
+-- On table names and first letter of name and the score
 CREATE INDEX idx_name_first_score ON names(name(1), score)
