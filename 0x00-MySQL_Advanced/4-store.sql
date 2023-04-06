6fdf0baadc36d41f4358f208e832fb391dd30d43
Ivy-Chelsea <105067998+Ivy-Chelsea@users.noreply.github.com>
2023-04-06 18:31:47 +0300

    add missing character

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,4 +1,4 @@
 -- Script that creates a trigger
 -- That decreases the quantity of an item after adding a new item
 CREATE TRIGGER decrease_q AFTER INSERT ON orders FOR EACH ROW
-UPDATE items SET quantity = quantity - NEW.number WHERE name=NEW.ite_name;
+UPDATE items SET quantity = quantity - NEW.number WHERE name=NEW.item_name;
