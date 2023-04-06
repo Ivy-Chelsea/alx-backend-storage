9fc640a4cdc469918936cdeb07d5c6ad885b245d
Ivy-Chelsea <105067998+Ivy-Chelsea@users.noreply.github.com>
2023-04-06 21:48:46 +0300

    table users

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -2,7 +2,7 @@
 -- with attributes id, email, name, country
 CREATE TABLE IF NOT EXISTS users
 (
-	id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	id int NOT NULL PRIMARY KEY AUTO INCREMENT,
 	email varchar(255) NOT NULL UNIQUE,
 	name varchar(255),
 	country ENUM('US', 'CO', 'IN') DEFAULT 'US' NOT NULL
