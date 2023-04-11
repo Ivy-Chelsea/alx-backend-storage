5df40f79c3cd1f8bfddbc7b1779e7e5ee53f2295
Ivy-Chelsea <ivychelsea20@gmail.com>
2023-04-11 21:12:54 +0300

    update topics

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Module containing function update_topics
+"""
+import pymongo
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    updates many rows
+    """
+    return mongo_collection.update_may(
+            {"name": name},
+            {"$set": {"topics": topics}}
+    )
